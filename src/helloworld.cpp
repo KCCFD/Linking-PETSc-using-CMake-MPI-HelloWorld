41bc524db44ad1f6543d9dd28f09c526c7524e23
Krishna Chandran <31917555+KCCFD@users.noreply.github.com>
2024-02-26 11:36:13 +0530

    Update helloworld.cpp

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,1 +1,22 @@
+/* This is the main function to call helloworld by MPI uing PETSc */
+
+#include<bits/stdc++.h>
+#include <petsc.h>
+
+int main( int argc,char **argv )
+{
+
+PetscErrorCode ierr;
+PetscMPIInt rank;
+
+PetscInitialize(&argc,&argv,PETSC_NULLPTR,PETSC_NULLPTR);
+
+ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank);CHKERRQ(ierr);
+
+PetscCallMPI(MPI_Comm_rank(PETSC_COMM_WORLD, &rank));
+
+
+PetscPrintf(PETSC_COMM_SELF,"Hello World from : \t %d\n",rank);
+return PetscFinalize();
+}
 
